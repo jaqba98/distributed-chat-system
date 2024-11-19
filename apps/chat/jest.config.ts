a7b1c42d74fa3486a883391e00ec8a5b7ffaadfb
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-11-19 20:37:41 +0100

    refactor: rename the server1 project to the chat project

--- apps/chat/jest.config.ts
+++ apps/chat/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'server1',
+  displayName: 'chat',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/server1',
+  coverageDirectory: '../../coverage/apps/chat',
 };
