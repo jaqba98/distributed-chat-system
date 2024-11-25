60b7f8ea0a32091c3e6a77eff3eee262be9a811e
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-11-25 14:01:42 +0100

    feat: create the server dto validation

--- libs/be/server/src/lib/const/message.const.ts
+++ libs/be/server/src/lib/const/message.const.ts
@@ -2,3 +2,6 @@ export const methodNotImplementedMsg = 'The method is not implemented!';
 
 export const envVarNotSetMsg = (env: string) =>
   `The ${env} environment variable not set!`;
+
+export const routeNotPropertySetMsg = (id: string, property: string) =>
+  `Route ${id} does not have the ${property} property set.`;
--- libs/be/server/src/lib/dom-service/check-server-dto.service.ts
+++ libs/be/server/src/lib/dom-service/check-server-dto.service.ts
@@ -1,13 +1,36 @@
 import { injectable } from 'tsyringe';
 
 import { ServerDtoModel } from '../model/dto/server-dto.model';
-import { envVarNotSetMsg } from '../const/message.const';
+import {
+  envVarNotSetMsg,
+  routeNotPropertySetMsg,
+} from '../const/message.const';
 import { SERVER_SOCKET_IO } from '../const/env.const';
 
 @injectable()
 export class CheckServerDtoService {
-  check(dto: ServerDtoModel): void {
+  check(dto: ServerDtoModel) {
+    this.checkRoutes(dto.routes);
     this.checkSocketIO(dto.socketIO);
+  }
+
+  private checkRoutes(routes: ServerDtoModel['routes']) {
+    const errors: string[] = [];
+    for (const route of routes) {
+      if (!route.method) {
+        errors.push(routeNotPropertySetMsg(route.id, 'method'));
+      }
+      if (!route.url) {
+        errors.push(routeNotPropertySetMsg(route.id, 'url'));
+      }
+      if (!route.controller) {
+        errors.push(routeNotPropertySetMsg(route.id, 'controller'));
+      }
+    }
+    if (errors.length === 0) return;
+    for (const error of errors) {
+      console.error(error);
+    }
   }
 
   private checkSocketIO(socketIO: ServerDtoModel['socketIO']) {
