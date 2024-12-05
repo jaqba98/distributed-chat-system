c6b86838b933190748797358410a74f4c09fba73
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-12-05 11:07:35 +0100

    feat: clean the app servcies

--- libs/be/server/src/index.ts
+++ libs/be/server/src/index.ts
@@ -2,4 +2,3 @@ export * from './lib/server';
 export * from './lib/controller/http.controller';
 export * from './lib/controller/socket-io.controller';
 export * from './lib/service/controller-decorator.service';
-export * from './lib/controller/http-404.controller';
--- libs/be/server/src/lib/server.ts
+++ libs/be/server/src/lib/server.ts
@@ -3,6 +3,6 @@ import { container } from 'tsyringe';
 
 import { ServerAppService } from './app-service/server-app.service';
 
-const server = container.resolve(ServerAppService);
-
-export const runServer = () => server.runServer();
+export const runServer = () => {
+  return container.resolve(ServerAppService).runServer();
+};
