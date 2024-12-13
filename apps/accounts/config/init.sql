f493eaaff498e62ff736533c12d921198645ae17
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-12-13 10:23:54 +0100

    refactor: application chat client configuration

--- apps/accounts/config/init.sql
+++ apps/accounts/config/init.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    nick VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL
 );
 
-INSERT INTO users (email, password) VALUES
-    ('user1@gmail.com', 'Pa$$word1'),
-    ('user2@gmail.com', 'Pa$$word2'),
-    ('user3@gmail.com', 'Pa$$word3');
+INSERT INTO users (nick, email, password) VALUES
+    ('user1', 'user1@gmail.com', 'Pa$$word1'),
+    ('user2', 'user2@gmail.com', 'Pa$$word2'),
+    ('user3', 'user3@gmail.com', 'Pa$$word3');
