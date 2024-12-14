eb241797dad3bcb5b7661572cc2330e0dbcac091
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-12-14 11:35:00 +0100

    feat: create basic login form

--- libs/fe/pages/src/lib/sign-in-page/sign-in-page.component.ts
+++ libs/fe/pages/src/lib/sign-in-page/sign-in-page.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-sign-in-page',
   standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sign-in-page.component.html',
   styleUrl: './sign-in-page.component.scss',
 })
-export class SignInPageComponent {}
+export class SignInPageComponent {
+  signInForm: FormGroup;
+
+  errorMessage = '';
+
+  constructor() {
+    this.signInForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signInForm);
+  }
+}
