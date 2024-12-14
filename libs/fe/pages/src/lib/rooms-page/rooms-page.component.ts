5bcd7bf0b1b6b9508d819904d65d7b222970efeb
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-12-14 13:42:37 +0100

    feat: turn on the chat docker compose configuration

--- libs/fe/pages/src/lib/rooms-page/rooms-page.component.ts
+++ libs/fe/pages/src/lib/rooms-page/rooms-page.component.ts
@@ -1,9 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { io, Socket } from 'socket.io-client';
 
 @Component({
   selector: 'lib-rooms-page',
   standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './rooms-page.component.html',
   styleUrl: './rooms-page.component.scss',
 })
-export class RoomsPageComponent {}
+export class RoomsPageComponent {
+  roomsForm: FormGroup;
+
+  private socket: Socket;
+
+  constructor() {
+    this.roomsForm = new FormGroup({
+      message: new FormControl(''),
+    });
+    this.socket = io('localhost:2000', {
+      transports: ['websocket'],
+    });
+    this.socket.on('connect', () => {
+      console.log('Connected to server:', this.socket.id);
+    });
+    this.socket.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+    this.socket.on('response', (data) => {
+      console.log(data);
+    });
+  }
+
+  onSubmit() {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const data: any = {
+      message: this.roomsForm.get('message')?.value,
+    };
+    this.socket.emit('message', data);
+  }
+}
