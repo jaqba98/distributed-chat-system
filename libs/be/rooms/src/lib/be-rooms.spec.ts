8ae30dc43e857ff435d64632c9dfda99e5434268
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-12-21 10:43:08 +0100

    feat: create rooms app and lib

---
+++ libs/be/rooms/src/lib/be-rooms.spec.ts
@@ -0,0 +1,7 @@
+import { beRooms } from './be-rooms';
+
+describe('beRooms', () => {
+  it('should work', () => {
+    expect(beRooms()).toEqual('be-rooms');
+  });
+});
---
+++ libs/be/rooms/src/lib/be-rooms.ts
@@ -0,0 +1,3 @@
+export function beRooms(): string {
+  return 'be-rooms';
+}
