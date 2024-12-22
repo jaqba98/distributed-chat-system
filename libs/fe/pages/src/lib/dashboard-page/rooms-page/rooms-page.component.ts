ef77f41212300bb7b8a59384917c931d07a61838
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-12-22 20:31:25 +0100

    feat: create basic table but with fake data

--- libs/fe/pages/src/lib/dashboard-page/rooms-page/rooms-page.component.ts
+++ libs/fe/pages/src/lib/dashboard-page/rooms-page/rooms-page.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+
+import { CustomerService } from './customer.service';
+import { Customer } from './customer';
 
 @Component({
   selector: 'lib-rooms-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TableModule],
   templateUrl: './rooms-page.component.html',
   styleUrl: './rooms-page.component.scss',
 })
-export class RoomsPageComponent {}
+export class RoomsPageComponent implements OnInit {
+  customers!: Customer[];
+
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit() {
+    this.customerService
+      .getCustomersLarge()
+      .then((customers) => (this.customers = customers));
+  }
+}
